2d807052bf693caa362fe4380e13f885aaca1666
alex <a.z.tvgg@icloud.com>
2022-03-08 19:02:10 +0000

    добавлен работающий мейн

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <QCoreApplication>
-
+#include <stack.h>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    stack<int> kek;
+    cout<<kek.Size()<<endl;
+    kek.Push(1);//add 1
+    kek.Push(2);// add 2
+    kek.Iter();// show stack
+    kek.Pop();// Вытащил последний элемент
+    cout<<endl;
+    kek.Iter();//show stack
+    stack<char> lol;
+    lol.Pop();//check except
+
 
     return a.exec();
 }
